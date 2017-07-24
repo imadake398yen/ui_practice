010dc17e48e1d35106e37d98944a55325ab7d959
falcon <imadake398yen@gmail.com>
2017-07-24 20:17:56 +0900

    いろいろ変えてみた

--- Assets/Scripts/ItemSelectController.cs
+++ Assets/Scripts/ItemSelectController.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemSelectController : MonoBehaviour {
 
+	[SerializeField] private Image iconImage;
 	[SerializeField] private List<Sprite> itemIcons;
 
+	[SerializeField]
 	private int currentSelect = 0;
 
 	private void Update () {
 		if ( Input.GetKeyDown(KeyCode.RightArrow) ) {
-			if (itemIcons.Count-1 < currentSelect) currentSelect += 1;
-			else currentSelect = 0;
+			currentSelect = (currentSelect + 1) % itemIcons.Count;
+			iconImage.sprite = itemIcons[currentSelect];
 		}
 		if ( Input.GetKeyDown(KeyCode.LeftArrow) ) {
 			if (currentSelect > 0) currentSelect -= 1;
 			else currentSelect = itemIcons.Count - 1;
+			iconImage.sprite = itemIcons[currentSelect];
 		}
 	}
 
