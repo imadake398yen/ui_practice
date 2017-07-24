96c0215f960644545fec622c258bee0d4b82f83a
falcon <imadake398yen@gmail.com>
2017-07-24 19:37:33 +0900

    first commit

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.SerializableAttribute]
+public class Item {
+
+	[SerializeField]
+	private string name;
+
+	[SerializeField]
+	private int id;
+
+}
---
+++ Assets/Scripts/ItemSelectController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSelectController : MonoBehaviour {
+
+	[SerializeField] private List<Sprite> itemIcons;
+
+	private int currentSelect = 0;
+
+	private void Update () {
+		if ( Input.GetKeyDown(KeyCode.RightArrow) ) {
+			if (itemIcons.Count-1 < currentSelect) currentSelect += 1;
+			else currentSelect = 0;
+		}
+		if ( Input.GetKeyDown(KeyCode.LeftArrow) ) {
+			if (currentSelect > 0) currentSelect -= 1;
+			else currentSelect = itemIcons.Count - 1;
+		}
+	}
+
+}
